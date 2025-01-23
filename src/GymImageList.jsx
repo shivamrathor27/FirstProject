a034825957635c5800edbaa768cb844f97a9d20b
Shivam Rathor <shivamrathor27@gmail.com>
2025-01-23 19:44:15 +0530

    Bug Fix

--- src/GymImageList.jsx
+++ src/GymImageList.jsx
@@ -5,7 +5,7 @@ export default function GymImageList() {
     <h1 style={{textAlign: "center"}}>Gallery</h1>
     <div className="image-container" id="gallery">
       {Array(9).fill().map((n,i)=>(
-        <img src={"src/assets/images/thegym" + (i+1)+".jpg"} alt="" className="image-item" />
+        <img src={"images/thegym" + (i+1)+".jpg"} alt="" className="image-item" />
       ))}
     </div>
     
