58570cde45fe6645477c29dfbfc0e9f6b268fc1f
shivamrathor27 <shivamrathor27@gmail.com>
2025-01-27 00:57:12 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import About from "./About";
-//import Gallery from "./Gallery";
+import Gallery from "./Gallery";
 import Home from "./Home";
 import Services from "./Services";
 
-export { Home, About, Services/*, Gallery*/ };
+export { Home, About, Services, Gallery };
